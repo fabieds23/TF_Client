8651f56c887860049e71eb3a0a3464be060b1fc4
fabieds23 <fzamora@estudiantec.cr>
2022-10-19 22:45:54 -0600

    Cliente0.2

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,4 +1,12 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
 public class Client {
+    private Socket socket;
+    private BufferedReader readerClient;
+    private BufferedWriter writerClient;
+
+
 }
--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,14 +1,53 @@
 package main;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Test!");
+    private Label clienteLabel;
+    @FXML
+    private Label esperaLabelCliente;
+    @FXML
+    private Button botonClient;
+    @FXML
+    private TextField textClient;
+
+    private Client client;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        try{
+            client = new Client(new Socket("localhost", 1234));
+            System.out.println("Conectado al servidor");
+        }
+        catch (IOException e){
+            System.out.println("Error");
+        }
+        client.receiveMessageFromServer(esperaLabelCliente);
+        botonClient.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String clientMessage = textClient.getText();
+                client.sendMessagetoServer(clientMessage);
+            }
+        });
+
     }
+    public static void labelUpdate(String messageFromServer, Label temp2) {
+        temp2.setText(messageFromServer);
+    }
+
 }
